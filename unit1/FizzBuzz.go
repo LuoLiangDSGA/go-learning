76047de343551f182a3d25851804dc3db65c9402
FrankLo <ll9420416@outlook.com>
2018-06-15 21:21:22 +0800

    :art: update code

--- unit1/FizzBuzz.go
+++ unit1/FizzBuzz.go
@@ -3,16 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	solution1();
+	fmt.Println("====solution1====")
+	solution1()
+	fmt.Println("====solution2====")
+	solution2()
 }
 
 func solution1() {
 	for i := 1; i <= 100; i++ {
-		if i % 3 == 0 && i % 5 == 0 {
+		if i%3 == 0 && i%5 == 0 {
 			fmt.Printf("FizzBuzz\n")
-		} else if i % 5 ==0 {
+		} else if i%5 == 0 {
 			fmt.Printf("Buzz\n")
-		} else if i % 3 == 0 && i % 5 == 0{
+		} else if i%3 == 0 && i%5 == 0 {
 			fmt.Printf("Fizz\n")
 		} else {
 			fmt.Printf("%d\n", i)
@@ -20,3 +23,28 @@ func solution1() {
 	}
 }
 
+func solution2() {
+	//定义常量
+	const (
+		Fizz = 3
+		Buzz = 5
+	)
+
+	var f bool
+	for i := 1; i <= 100; i++ {
+		f = false
+		if i%Fizz == 0 {
+			fmt.Printf("Fizz")
+			f = true
+		}
+		if i%Buzz == 0 {
+			fmt.Printf("Buzz")
+			f = true
+		}
+		if !f {
+			fmt.Printf("%d", i)
+		}
+		fmt.Println()
+	}
+
+}
