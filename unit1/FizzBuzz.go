902f4d7b6de4e9e1901b8221da9159460d4dce84
FrankLo <ll9420416@outlook.com>
2018-06-15 20:56:18 +0800

    :art: update code

--- unit1/FizzBuzz.go
+++ unit1/FizzBuzz.go
@@ -3,6 +3,10 @@ package main
 import "fmt"
 
 func main() {
+	solution1();
+}
+
+func solution1() {
 	for i := 1; i <= 100; i++ {
 		if i % 3 == 0 && i % 5 == 0 {
 			fmt.Printf("FizzBuzz\n")
@@ -15,3 +19,4 @@ func main() {
 		}
 	}
 }
+
