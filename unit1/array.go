02ba2943747fce08163575dd568553c54648723f
FrankLo <ll9420416@outlook.com>
2018-11-12 14:23:38 +0800

    :tada: review code

---
+++ unit1/array.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func array() {
+	var arr [10]int
+	arr[0] = 42
+	arr[1] = 13
+	fmt.Printf("The First element is %d\n", arr[0])
+}
+
+func array1() {
+	a := [3]int{1, 2, 3}
+	// go会自动统计元素的个数
+	b := [...]int{1, 2, 3}
+	c := [3][2]int{{1, 2}, {3, 4}, {5, 6}}
+	// create slice
+	s1 := a[2:4]
+}
+
+func main() {
+	array()
+}
