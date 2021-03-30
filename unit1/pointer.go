86e455b5c8b421e5c2227f7aa866536cdf7497c9
luoliang.fantasy <luoliang.fantasy@bytedance.com>
2021-03-30 10:56:26 +0800

    pointer practice

--- unit1/pointer.go
+++ unit1/pointer.go
@@ -4,6 +4,12 @@ import (
 	"fmt"
 )
 
+// 结构体
+type Person struct {
+	name string
+	age  int8
+}
+
 func pointer1() {
 	i, j := 10, 20
 	p := &i
@@ -15,6 +21,20 @@ func pointer1() {
 	fmt.Println(j)
 }
 
+//结构体练习
+func struct1() {
+	fmt.Println(Person{"fantasy", 1})
+	v := Person{"fantasy", 1}
+	v.name = "yehuimei"
+	fmt.Println(v)
+	fmt.Println(Person{})
+	p := &v
+	fmt.Println(*p)
+	(*p).name = "jay chou" // 也可以通过p.name = "" 来设置值
+	fmt.Println(v)
+}
+
 func main() {
 	pointer1()
+	struct1()
 }
