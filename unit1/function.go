7cb9e2320b5c32670fc6abcdf37f2a058f876a3a
FantasyLo <ll9420416@outlook.com>
2021-03-31 15:13:05 +0800

    Update function.go

--- unit1/function.go
+++ unit1/function.go
@@ -18,6 +18,27 @@ func adder() func(int) int {
 	}
 }
 
+// 返回一个“返回int的函数”
+//`(0, 1, 1, 2, 3, 5, ...)`。
+func fibonacci() func() int {
+	a := 0
+	b := 1
+	i := 0
+	return func() int {
+		tmp := a + b
+		if i == 0 {
+			i++
+			return 0
+		}
+		if i > 1 {
+			a = b
+			b = tmp
+		}
+		i++
+		return tmp
+	}
+}
+
 func main() {
 	hypot := func(x, y float64) float64 {
 		return math.Sqrt(x*x + y*y)
@@ -29,5 +50,10 @@ func main() {
 	a, b := adder(), adder()
 	for i := 0; i < 10; i++ {
 		fmt.Println(a(i), b(-2*i))
+	}
+
+	f := fibonacci()
+	for i := 0; i < 10; i++ {
+		fmt.Println(f())
 	}
 }
