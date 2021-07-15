2e4aca0ad2fbd26d1ca45b6167b578a3339efa59
luoliang.fantasy <ll9420416@outlook.com>
2021-07-15 16:39:27 +0800

    update code

--- etcd/go.mod
+++ etcd/go.mod
@@ -3,7 +3,7 @@ module github.com/golang-learning/etcd
 go 1.15
 
 require (
-	github.com/coreos/etcd v3.3.25+incompatible // indirect
+	github.com/coreos/etcd v3.3.25+incompatible
 	github.com/coreos/go-systemd v0.0.0-20191104093116-d3cd4ed1dbcf // indirect
 	github.com/coreos/pkg v0.0.0-20180928190104-399ea9e2e55f // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
--- etcd/main.go
+++ etcd/main.go
@@ -25,6 +25,115 @@ func main() {
 	//初始化kv
 	kvc := clientv3.NewKV(cli)
 	//获取值
+	//getVal(rootBackground, err, kvc)
+	//设置值
+	//putVal(rootBackground, err, kvc)
+	//续租测试
+	leaseKV(cli, rootBackground, kvc)
+	// 循环模拟kv变化
+	//watchKV(kvc, err, cli)
+
+}
+
+func watchKV(kvc clientv3.KV, err error, cli *clientv3.Client) {
+	go func() {
+		for i := 0; i < 10; i++ {
+			fmt.Println("key: /aa/bb, put val cc")
+			kvc.Put(context.TODO(), "/aa/bb", "cc")
+			fmt.Println("delete key /aa/bb")
+			kvc.Delete(context.TODO(), "/aa/bb")
+			time.Sleep(1 * time.Second)
+		}
+	}()
+	getResp, err := kvc.Get(context.TODO(), "/aa/bb")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	// key存在
+	if len(getResp.Kvs) > 0 {
+		fmt.Printf("current value is %s", string(getResp.Kvs[0].Value))
+	}
+	watchRevision := getResp.Header.Revision + 1
+	watcher := clientv3.NewWatcher(cli)
+	fmt.Println("listen backwards from the current version：", watchRevision)
+	watchChannel := watcher.Watch(context.TODO(), "/aa/bb", clientv3.WithRev(watchRevision))
+	// 处理kv变化
+	for watchResp := range watchChannel {
+		for _, event := range watchResp.Events {
+			switch event.Type {
+			case 0:
+				fmt.Println("update->", string(event.Kv.Value), "revision:", event.Kv.CreateRevision, event.Kv.ModRevision)
+			case 1:
+				fmt.Println("delete->", "revision：", event.Kv.ModRevision)
+			}
+		}
+	}
+}
+
+func leaseKV(cli *clientv3.Client, rootBackground context.Context, kvc clientv3.KV) {
+	lease := clientv3.NewLease(cli)
+	ctx, _ := context.WithTimeout(rootBackground, time.Duration(100)*time.Second)
+	// 创建一个租约
+	grant, _ := lease.Grant(ctx, 2)
+	leaseId := grant.ID
+	if _, err := kvc.Put(ctx, "/aa/bb", "cc", clientv3.WithLease(leaseId)); err != nil {
+		fmt.Println(err)
+		return
+	}
+	// 自动续约
+	aliveCh, err := lease.KeepAlive(context.TODO(), leaseId)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	go func() {
+		for {
+			select {
+			case keepResp := <-aliveCh:
+				if keepResp == nil {
+					fmt.Println("lease is expired")
+					goto End
+				} else {
+					fmt.Println("receive auto lease ack：", keepResp.ID)
+				}
+			}
+		}
+	End:
+	}()
+	for {
+		getResp, err := kvc.Get(ctx, "/aa/bb")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		if getResp.Count == 0 {
+			fmt.Println("kv expired")
+			break
+		}
+		fmt.Println("haven't expired")
+		// 延迟一秒执行
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func putVal(rootBackground context.Context, err error, kvc clientv3.KV) {
+	uuid := uuid.New().String()
+	fmt.Printf("new value is :%s\r\n", uuid)
+	ctx, cancelFunc2 := context.WithTimeout(rootBackground, time.Duration(2)*time.Second)
+	_, err = kvc.Put(ctx, "cc", uuid)
+	if delRes, err := kvc.Delete(ctx, "cc"); err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Printf("delete %s for %t\n", "cc", delRes.Deleted > 0)
+	}
+	cancelFunc2()
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func getVal(rootBackground context.Context, err error, kvc clientv3.KV) {
 	ctx, cancelFunc := context.WithTimeout(rootBackground, time.Duration(2)*time.Second)
 	response, err := kvc.Get(ctx, "cc")
 	cancelFunc()
@@ -36,18 +145,5 @@ func main() {
 		fmt.Printf("last value is :%s\r\n", string(kvs[0].Value))
 	} else {
 		fmt.Printf("empty key for %s\n", "cc")
-	}
-	uuid := uuid.New().String()
-	fmt.Printf("new value is :%s\r\n", uuid)
-	ctx2, cancelFunc2 := context.WithTimeout(rootBackground, time.Duration(2)*time.Second)
-	_, err = kvc.Put(ctx2, "cc", uuid)
-	if delRes, err := kvc.Delete(ctx2, "cc"); err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Printf("delete %s for %t\n", "cc", delRes.Deleted > 0)
-	}
-	cancelFunc2()
-	if err != nil {
-		fmt.Println(err)
 	}
 }
